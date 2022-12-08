65a1e702036dfc24c49219004933e494781b6315
Remy <remy@chaoticmewtral.com>
2022-12-08 15:47:50 -0500

    Add to files

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const game = (function()  {
+    const players = [];
     const board = [];
 
     //cache DOM
 
     //bind events
+
+    //objects
+    const player = function(name, mark) {
+        this.name = name;
+        this.mark = mark;
+    }
 })();
 
 /* 
