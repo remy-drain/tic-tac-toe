e9ca585c3d01a7a2daab3861396d48213960ce30
Remy <remy@chaoticmewtral.com>
2022-12-22 21:55:19 -0500

    Add to branch-script
    Wrote functions for addMark. Passed values to displayControl.

--- branch-script.js
+++ branch-script.js
@@ -8,9 +8,25 @@
     const gameBoard = {
        gameBoard: [],
 
+       addMark: function(e) {
+        let cell = e.target.getAttribute("data-cell");
+        let player = board.getAttribute("class");
+        
+        const mark = {
+            cell: cell,
+            player: player
+        }
+        gameBoard.gameBoard.push(mark);
+        displayControl.showMark(cell, player);
+       }
     }
 
     const displayControl = {
-
+        showMark: function(cell, player) {
+            
+        }
     }
+
+    //event listeners
+    cells.forEach(cell => cell.addEventListener("click", gameBoard.addMark));
 })();
