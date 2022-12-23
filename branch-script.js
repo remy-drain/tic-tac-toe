d2ad6f4b82db41ce5de689bcafc5a35fcc99c001
Remy <remy@chaoticmewtral.com>
2022-12-22 20:47:50 -0500

    Add to files

--- branch-script.js
+++ branch-script.js
@@ -1,6 +1,9 @@
 (function() {
     //cache DOM
-    
+    const page = document.querySelector("body");
+    const players = page.querySelector("#players");
+    const board = page.querySelector("#board");
+    const cells = board.querySelectorAll(".cell");
 
     const gameBoard = {
        gameBoard: [],
