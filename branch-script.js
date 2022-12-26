425c62878c71ca27dc4719fa9d64e382b5acc40e
Remy <remy@chaoticmewtral.com>
2022-12-26 14:03:27 -0500

    Add to b-script

--- branch-script.js
+++ branch-script.js
@@ -1,12 +1,25 @@
 (function() {
     //cache DOM
     const page = document.querySelector("body");
-    const players = page.querySelector("#players");
     const board = page.querySelector("#board");
     const cells = board.querySelectorAll(".cell");
 
+    const winningArrays = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
     const gameBoard = {
-       gameBoard: [],
+       gameBoard: {
+        x: [],
+        oh: []
+       },
 
        checkCell: function(e) {
         let selection = e.target;
@@ -18,16 +31,16 @@
         }
        },
        addMark: function(e) {
-        const mark = {
-            [e.getAttribute("data-cell")]: board.getAttribute("class")
-        }
-        this.gameBoard.push(mark);
+        const cell = e.getAttribute("data-cell");
+        this.gameBoard[board.getAttribute("class")].push(cell);
+        this.gameBoard.checkForWin();
        },
        checkForWin: function() {
         if (this.gameBoard.length < 5) {
             this.switchPlayers();
         } else {
-            this.gameBoard.filter();
+            let currentPlayer = board.getAttribute("class");
+            winningArrays.forEach(arr => this.gameBoard[currentPlayer].includes(arr))
         }
        },
        switchPlayers: function() {
