9833e400d9e2c1aab7fe13d2860847e0aa97d309
Remy <remy@chaoticmewtral.com>
2023-01-03 18:21:37 -0500

    Add to files
    Finished.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (function() {
+    let playerArray = [];
     let boardArr = Array(9).fill(null);
     const winningArrays = [
         [0,1,2],
@@ -13,7 +14,13 @@
     // cache DOM
     const game = document.querySelector("#game");
     const cells = Array.from(game.querySelectorAll(".cell"));
-    const players = game.querySelectorAll(".player");
+    const playerNames = game.querySelector("#player-names");
+    const playerX = playerNames.querySelector("#x-name");
+    const playerO = playerNames.querySelector("#o-name");
+    const players = game.querySelector("#players");
+    const xName = players.querySelector(".x");
+    const oName = players.querySelector(".o");
+    const startGame = playerNames.querySelector("#start");
     const endGame = game.querySelector("#game-over");
     const msg = endGame.querySelector("#message");
     const playAgain = endGame.querySelector("#restart");
@@ -30,16 +37,19 @@
             }
         },
         checkWin: function() {
+            let isWinner = false;
             for (let arr of winningArrays) {
                 const [a,b,c] = arr;
                 if (boardArr[a] == game.classList.value && boardArr[b] == game.classList.value && boardArr[c] == game.classList.value) {
-                    this.gameOver("win");
+                    isWinner = true;
                 }
             }
-            if (boardArr.includes(null)) {
-                this.switchPlayers();
-            } else {
+            if (isWinner) {
+                this.gameOver("win");
+            } else if (!boardArr.includes(null)) {
                 this.gameOver("draw");
+            } else {
+                this.switchPlayers();
             }
         },
         switchPlayers: function() {
@@ -54,8 +64,8 @@
         gameOver: function(e) {
             let endMessage;
             if (e == "win") {
-                let winner = game.classList.value.toUpperCase();
-                endMessage = winner + " wins!";
+                let winner = playerArray.find((obj) => {return obj.mark === game.classList.value});
+                endMessage = winner.name + " wins!";
             } else if (e == "draw") {
                 endMessage = "It's a tie!";
             }
@@ -71,6 +81,17 @@
     }
 
     const displayControl = {
+        showPlayers: function(x, o) {
+            xName.append(`: ${x}`);
+            oName.append(`: ${o}`);
+
+            this.hidePlayerInput();
+        },
+        hidePlayerInput: function() {
+            playerX.value = '';
+            playerO.value = '';
+            playerNames.classList.remove("show");
+        },
         showMark: function(cell) {
             cell.classList.add(game.classList.value);
             gameBoard.checkWin();
@@ -79,6 +100,25 @@
             cells.forEach(cell => cell.className = 'cell');
         }
     }
+
+    const playerControl = {
+        getPlayers: function() {
+            let x = {
+                mark: "x",
+                name: playerX.value
+            };
+            let o = {
+                mark: "o",
+                name: playerO.value
+            };
+            playerArray.push(x);
+            playerArray.push(o);
+
+            displayControl.showPlayers(x.name, o.name);
+        }
+    }
+    startGame.addEventListener("click", playerControl.getPlayers);
+
     cells.forEach(cell => cell.addEventListener("click", gameBoard.addMark));
 
     playAgain.addEventListener("click", gameBoard.restart);
